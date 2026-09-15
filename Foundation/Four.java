0b34897fd5f7c55eebc1425807664057e0cb74b2
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-09-15 22:22:10 +0530

    Data Types

---
+++ Foundation/Four.java
@@ -0,0 +1,26 @@
+public class Four{
+    public static void main(String[] args){
+        //types of datatypes;
+        //primitive and non primitive;
+        byte c=22; //1byte
+        short d=33; //2 byte
+        int a=2; //4 byte
+        float b=3.3f; //4  byte
+        long e=394394794L; //4 byte
+        double f=2.398457393274732; //8 byte;
+        char g='A';// 2 byte;
+        boolean h=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+
+
+
+    }
+}
---
+++ Foundation/second.java
@@ -0,0 +1,21 @@
+//Variable:variable is a container which is used to store value inside it and the amount of memory it required depends upon the type of data it is storing
+//Datatypes: defines what types of value a variable is holding ;
+
+public class second{
+    public static void main(String[] args){
+        int a=2;
+        long c=387777273737397L;
+        float b=3.3f;
+        double d=4.83929289398;
+        char e='R';
+        String f="Rupesh Kumar Chaudhary";
+        System.out.println(a);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        
+
+    }
+}
