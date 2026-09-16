5c9a5b692abba559ac82d84032bf1a8c62b976a1
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-09-16 23:02:35 +0530

    assignmnet operator

---
+++ Foundation/Six.java
@@ -0,0 +1,32 @@
+public class Six{
+    public static void main(String[] args){
+        //operators;
+        //assignment operators:+,-,*,/,%;
+        int a=2;
+        int b=3;
+        System.out.println("a+b="+(a+b));
+        System.out.println("a-b="+(a-b));
+        System.out.println("a*b="+(a*b));
+        System.out.println("a/b="+(a/b));
+        System.out.println("a%b="+(a%b));
+
+        //pre increment and decrement;
+        int c=4;
+        int d=++c;
+        System.out.println("d="+d); //5
+        int e=--d;
+        System.out.println("e="+e); // 4
+
+
+        //post increment;
+
+        int f=6;
+        System.out.println("f="+(f++));
+        System.out.println(f);
+
+        int g=8;
+        System.out.println("g="+(g--));
+        System.out.println(g);
+
+    }
+}
