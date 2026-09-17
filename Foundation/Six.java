59139aa03550c8faada3963eccf74b23fa52fe99
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-09-17 09:37:12 +0530

    operators in java

--- Foundation/Six.java
+++ Foundation/Six.java
@@ -28,7 +28,25 @@ public class Six{
         System.out.println("g="+(g--));
         System.out.println(g);
 
-        //re
+        //relational operator;
+
+        int x=2;
+        int y=4;
+        System.out.println("x>y="+(x>y));
+        System.out.println("x<y="+(x<y));
+        System.out.println("x==y="+(x==y));
+        System.out.println("x!=y="+(x!=y));
+        System.out.println("x>=y="+(x>=y));
+        System.out.println("x<=y="+(x<=y));
+
+        //assignment operators;
+
+        int r=8;
+        int s=9;
+        System.out.println("r="+(r+=2));
+        System.out.println("s="+(s-=1));
+        System.out.println("s="+(s*=3));
+        System.out.println("r="+(r/=2));
 
     }
 }
