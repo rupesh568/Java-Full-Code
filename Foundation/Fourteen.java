b7ab63d5124403693a267cffe9686642d5814b59
Rupesh Kumar Chaudhary <rupeshjaiswal999939@gmail.com>
2026-09-25 00:03:08 +0530

    Break and Continue

---
+++ Foundation/Fourteen.java
@@ -0,0 +1,33 @@
+public class Fourteen{
+    public static void main(String[] args){
+        int i=1;
+        while(i<=10){
+            if(i==4){
+                break;
+
+            }
+            System.out.println(i);
+            i++;
+        }
+
+        int j=1;
+        while(j<=20){
+            if(j==5){
+                j+=1;
+                continue;
+            }
+            System.out.println(j);
+            j++;
+        }
+
+        int k=1;
+        while(k<=10){
+            if(k==2){
+                k+=2;
+                continue;
+            }
+            System.out.println(k);
+            k++;
+        }
+    }
+}
